e0cce6270858189e4087c65f5f93eca2678fe964
Bowuigi <70549864+Bowuigi@users.noreply.github.com>
2021-03-31 15:55:23 -0300

    Updated to version 1.0
    First release, now it is usable despite using 17.9MB of storage.

--- runmicrolinux.sh
+++ runmicrolinux.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/sh
-qemu-system-x86_64 -net nic -net user --kernel /usr/src/linux/linux-5.11.1/arch/x86_64/boot/bzImage -initrd ~/initramfs.cpio.gz --append "root=/dev/ram init=/init"
+qemu-system-x86_64 -net nic -net user --kernel bzImage -initrd initramfs.cpio.gz --append "root=/dev/ram init=/init"
